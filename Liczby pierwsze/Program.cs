516c5a9706b7ccb1971569a1c24c55ff54152671
Mateusz Jaroszko <matt7269@protonmail.com>
2021-10-23 21:25:41 +0200

    działa ale bez dwójki

--- Liczby pierwsze/Program.cs
+++ Liczby pierwsze/Program.cs
@@ -4,36 +4,49 @@ namespace Liczby_pierwsze
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            
-            licz(Convert.ToInt32(Console.ReadLine())); 
-        static void licz(int num)
+            int ile = Convert.ToInt32(Console.ReadLine());
+            int p;
+            int i = 0;
+            int counter = 0; 
+            while(counter <ile) 
             {
-                
-               
-                    bool pierwsza = true;
-                    for (int i = 2; i < num; i++)
-                    {
-                        if (num % i == 0)
-                        {
-                            pierwsza = false;
-
-                        }
-                        if (pierwsza)
-                        {
-                            Console.WriteLine("Jest liczbą pierwszą");
-                            break;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Nie jest liczbą pierwszą");
-                            break;
-                        }
-                    }
-                
+                int wynik = czyPierwsza(i);
+                i += 1;
+                if (wynik != 0)
+                {
+                    counter += 1;
+                    Console.WriteLine(wynik);
+                }
             }
-        }
+            
+             
 
+
+
+        }
+        public static int czyPierwsza(int num)
+        {
+
+            bool pierwsza = true;
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    pierwsza = false;
+
+                }
+                if (pierwsza)
+                {
+                    return num;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            return 0;
+        }
     }
 }
